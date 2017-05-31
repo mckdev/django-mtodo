b8b444a22655ba456d769c7ea3976fad61e1833c
Maciej Lorenc <lorencmaciek@gmail.com>
2017-06-01 01:23:57 +0200

    Made all views login required

--- django_mtodo/urls.py
+++ django_mtodo/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^mtodo/', include('mtodo.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^mtodo/admin/', admin.site.urls),
+
+    url(r'^accounts/login/$', auth_views.login, name='login'),
 ]
--- mtodo/views.py
+++ mtodo/views.py
@@ -1,44 +1,46 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.core import serializers
 from .models import Todo
 
-
+@login_required
 def index(request):
     return render(request, 'mtodo/index.html')
 
-
+@login_required
 def todo_list(request):
     todos = serializers.serialize('json', Todo.objects.order_by('-pk'))
     return HttpResponse(todos, content_type='application/json')
 
-
+@login_required
 def add_todo(request):
     todo = Todo.objects.create(todo_text=request.POST['todo_text'])
     todo.save()
     return HttpResponse()
 
-
+@login_required
 def change_todo(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.todo_text = todo_text=request.POST['todo_text']
     todo.save()
     return HttpResponse()
 
-
+@login_required
 def delete_todo(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.delete()
     return HttpResponse()
 
+@login_required
 def toggle_completed(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.completed = not todo.completed
     todo.save()
     return HttpResponse()
 
-
+@login_required
 def toggle_all(request):
     total_todos = len(Todo.objects.all())
     # Get number of completed todos
